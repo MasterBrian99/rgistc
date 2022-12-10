820712122d7819880a031bc17efe871d37a3d1dd
Pasindu P Konghawaththa <pasindudixon25@gmail.com>
2022-12-11 01:16:46 +0530

    add main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
+use std::{fs, path::Path};
+
+use clap::{builder::Str, Parser};
+
+// Simple to program to create github Gists
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    //File Path
+    #[arg(short, long)]
+    file: String,
+
+    //Github Token
+    #[arg(short, long,default_value_t =String::from(""))]
+    key: String,
+
+    // Gist Description
+    #[arg(short, long, default_value_t =String::from(""))]
+    description: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    if !Path::new(&args.file).exists() {
+        println!("No File");
+        return;
+    }
+
+    let content = match fs::read_to_string(&args.file) {
+        Result::Ok(value) => value,
+        Result::Err(error) => panic!("{}", error),
+    };
+    create_gist(
+        content,
+        Path::new(&args.file).file_name().unwrap().to_str().unwrap(),
+    )
+}
+
+fn create_gist(content: String, file: &str) {
+    println!("{}", content);
+    println!("{}", file)
 }
